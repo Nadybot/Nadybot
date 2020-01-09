2c70fd1a38c3413c72c8c0ea410773c0fc15665b
Nadyita <nadyita@hodorraid.org>
2020-01-09 10:50:37 +0100

    Removing (now) outdated docs as apigen does not work on any recent PHP version

--- tools/generate_api.bat
+++
@@ -1,4 +0,0 @@
-cd ..
-.\win32\php -c php-win.ini ./tools/apigen.phar --version
-.\win32\php -c php-win.ini ./tools/apigen.phar generate --access-levels=public,protected,private --title="Budabot 3.5_GA Docs" --source=./core,./modules -d ./docs/api --debug
-pause
--- tools/generate_api.sh
+++
@@ -1,3 +0,0 @@
-cd ..
-php ./tools/apigen.phar --version
-php ./tools/apigen.phar generate --access-levels=public,protected,private --title="Budabot 4.Beta Docs" --source=./core,./modules -d ./docs/api --debug
