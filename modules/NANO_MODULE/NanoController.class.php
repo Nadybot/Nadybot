7daed1da9d4eca351b2f94f1f41a42a69b13b163
Nadyita <nadyita@hodorraid.org>
2020-06-02 02:43:10 +0200

    Support for listing general and froob-friendly nanos sub-strains are now shown with !nano command matches

--- modules/NANO_MODULE/NanoController.class.php
+++ modules/NANO_MODULE/NanoController.class.php
@@ -26,6 +26,13 @@ namespace Budabot\User\Modules;
  *		alias		= 'nl'
  *	)
  *	@DefineCommand(
+ *		command     = 'nanolinesfroob',
+ *		accessLevel = 'all',
+ *		description = 'Shows nanos for froobs based on nanoline ',
+ *		help        = 'nanolinesfroob.txt',
+ *		alias		= 'nlf'
+ *	)
+ *	@DefineCommand(
  *		command     = 'nanoloc',
  *		accessLevel = 'all',
  *		description = 'Browse nanos by location',
@@ -97,13 +104,15 @@ class NanoController {
 				location,
 				professions,
 				school,
-				strain AS nanoline_name
+				strain AS nanoline_name,
+				sub_strain
 			FROM
 				nanos
 			WHERE
 				$query
 			ORDER BY
 				strain ASC,
+				sub_strain ASC,
 				ql DESC,
 				nano_cost DESC,
 				nano_name LIKE 'Improved%' DESC,
@@ -119,15 +128,21 @@ class NanoController {
 		} else {
 			$blob = '';
 			$currentNanoline = -1;
+			$currentSubstrain = null;
 			foreach ($data as $row) {
-				if ($currentNanoline !== $row->nanoline_name) {
+				if ($currentNanoline !== $row->nanoline_name || $currentSubstrain !== $row->sub_strain) {
 					if (!empty($row->nanoline_name)) {
 						$nanolineLink = $this->text->makeChatcmd("see all nanos", "/tell <myname> nanolines $row->nanoline_name");
-						$blob .= "\n<header2>$row->school<end> &gt; <header2>$row->nanoline_name<end> - [$nanolineLink]\n";
+						$blob .= "\n<header2>$row->school<end> &gt; <header2>$row->nanoline_name<end>";
+						if ($row->sub_strain) {
+							$blob .= " &gt; <header2>$row->sub_strain<end>";
+						}
+						$blob .= " - [$nanolineLink]\n";
 					} else {
 						$blob .= "\n<header2>Unknown/General<end>\n";
 					}
 					$currentNanoline = $row->nanoline_name;
+					$currentSubstrain = $row->sub_strain;
 				}
 				$nanoLink = $this->makeNano($row->nano_id, $row->nano_name);
 				$crystalLink = $this->text->makeItem($row->crystal_id, $row->crystal_id, $row->ql, "Crystal");
@@ -150,12 +165,44 @@ class NanoController {
 	 * @Matches("/^nanolines$/i")
 	 */
 	public function nanolinesListProfsCommand($message, $channel, $sender, $sendto, $args) {
-		$sql = "SELECT DISTINCT professions FROM nanos WHERE professions NOT LIKE '%:%' ORDER BY professions ASC";
+		$this->listNanolineProfs($sendto, false);
+	}
+
+	/**
+	 * @HandlesCommand("nanolinesfroob")
+	 * @Matches("/^nanolinesfroob$/i")
+	 */
+	public function nanolinesFroobListProfsCommand($message, $channel, $sender, $sendto, $args) {
+		$this->listNanolineProfs($sendto, true);
+	}
+
+	/**
+	 * List all professions for which nanolines exist
+	 *
+	 * @param \Budabot\Core\CommandReply $sendto Where to send the reply to
+	 * @param bool $froobObly Is set, only show professions a froob can play
+	 * @return void
+	 */
+	public function listNanolineProfs($sendto, $froobOnly) {
+		$froobWhere = "";
+		if ($froobOnly) {
+			$froobWhere = "AND professions NOT IN ('Keeper', 'Shade')";
+		}
+		$sql = "SELECT
+			DISTINCT professions
+			FROM
+				nanos
+			WHERE
+				professions NOT LIKE '%:%'
+				$froobWhere
+			ORDER BY
+				professions ASC";
 		$data = $this->db->query($sql);
 
 		$blob = "<header2>Choose a profession<end>\n";
+		$command = $froobOnly ? "nanolinesfroob" : "nanolines";
 		foreach ($data as $row) {
-			$blob .= "<tab>" . $this->text->makeChatcmd($row->professions, "/tell <myname> nanolines $row->professions");
+			$blob .= "<tab>" . $this->text->makeChatcmd($row->professions, "/tell <myname> $command $row->professions");
 			$blob .= "\n";
 		}
 		$blob .= $this->getFooter();
@@ -169,23 +216,56 @@ class NanoController {
 	 * @Matches("/^nanolines (.+)$/i")
 	 */
 	public function nanolinesListCommand($message, $channel, $sender, $sendto, $args) {
+		$this->listNanolines($sendto, false, $args);
+	}
+
+	/**
+	 * @HandlesCommand("nanolinesfroob")
+	 * @Matches("/^nanolinesfroob (.+)$/i")
+	 */
+	public function nanolinesFroobListCommand($message, $channel, $sender, $sendto, $args) {
+		$this->listNanolines($sendto, true, $args);
+	}
+
+	public function listNanolines($sendto, $froobOnly, $args) {
 		$args[1] = html_entity_decode($args[1]);
 		$nanoArgs = explode(" > ", $args[1]);
 		$profArg = array_shift($nanoArgs);
 		$profession = $this->util->getProfessionName($profArg);
+		if (in_array($profArg, array("general", "General"))) {
+			$profession = "General";
+		}
 		if ($profession === '') {
-			$this->nanolinesShow($args[1], null, $sendto);
+			$this->nanolinesShow($args[1], null, $froobOnly, $sendto);
 		} elseif (count($nanoArgs)) {
-			$this->nanolinesShow(implode(" > ", $nanoArgs), $profession, $sendto);
+			$this->nanolinesShow(implode(" > ", $nanoArgs), $profession, $froobOnly, $sendto);
 		} else {
-			$this->nanolinesList($profession, $sendto);
+			$this->nanolinesList($profession, $froobOnly, $sendto);
 		}
 	}
 	
-	private function nanolinesShow($nanoline, $prof, $sendto) {
+	/**
+	 * Show all nanos of a nanoline grouped by sub-strain
+	 *
+	 * @param string $nanoline   The full name of the nanoline to show
+	 * @param string $prof       If set, only show nanos that profession can use
+	 * @param bool   $froobOnly  If true, only show nanos a froob can use
+	 * @param \Budabot\Core\CommandReply $sendto Object to send the reply to
+	 * @return void
+	 */
+	private function nanolinesShow($nanoline, $prof, $froobOnly, $sendto) {
 		$profWhere = "";
+		$froobWhere = "";
 		if ($prof !== null) {
 			$profWhere = "AND professions LIKE ?";
+		}
+		if ($froobOnly) {
+			if ($prof !== null && in_array($prof, array("Keeper", "Shade"))) {
+				$msg = "<highlight>$prof<end> is not playable as froob.";
+				$sendto->reply($msg);
+				return;
+			}
+			$froobWhere = "AND froob_friendly IS TRUE";
 		}
 		$sql = "SELECT * 
 			FROM
@@ -193,6 +273,7 @@ class NanoController {
 			WHERE
 				strain = ?
 			$profWhere
+			$froobWhere
 			ORDER BY
 				sub_strain ASC,
 				ql DESC,
@@ -234,20 +315,55 @@ class NanoController {
 		$sendto->reply($msg);
 	}
 
-	private function nanolinesList($profession, $sendto) {
-		$sql = "SELECT distinct school,strain FROM nanos WHERE professions LIKE ? GROUP BY school,strain ORDER BY school ASC, strain ASC";
+	/**
+	 * List all nanolines for a profession, grouped by school
+	 *
+	 * @param string $profession The full name of the profession
+	 * @param bool   $froobOnly  If true, only show nanolines containing nanos a froob can use
+	 * @param \Budabot\Core\CommandReply $sendto Object to send the reply to
+	 * @return void
+	 */
+	private function nanolinesList($profession, $froobOnly, $sendto) {
+		$froobWhere = "";
+		if ($froobOnly) {
+			$froobWhere = "AND froob_friendly IS TRUE";
+			if ($profession !== null && in_array($profession, array("Keeper", "Shade"))) {
+				$msg = "<highlight>$profession<end> is not playable as froob.";
+				$sendto->reply($msg);
+				return;
+			}
+		}
+		$sql = "SELECT
+			distinct school,strain
+		FROM
+			nanos
+		WHERE
+			professions LIKE ?
+		$froobWhere
+		GROUP BY
+			school,strain
+		ORDER BY
+			school ASC,
+			strain ASC";
 		$data = $this->db->query($sql, "%${profession}%");
 
-		$shortProf = $this->util->getProfessionAbbreviation($profession);
+		$shortProf = $profession;
+		if ($profession !== 'General') {
+			$shortProf = $this->util->getProfessionAbbreviation($profession);
+		}
 		$blob = '';
 		$lastSchool = null;
+		$command = "nanolines";
+		if ($froobWhere) {
+			$command = "nanolinesfroob";
+		}
 		foreach ($data as $row) {
 			$strain = $row->strain;
 			if ($lastSchool === null || $lastSchool !== $row->school) {
-				$blob .= "<header2>{$row->school}<end>\n";
+				$blob .= "<pagebreak><header2>{$row->school}<end>\n";
 				$lastSchool = $row->school;
 			}
-			$blob .= "<tab>" . $this->text->makeChatcmd($strain, "/tell <myname> nanolines $shortProf > $row->strain");
+			$blob .= "<tab>" . $this->text->makeChatcmd($strain, "/tell <myname> $command $shortProf > $row->strain");
 			$blob .= "\n";
 		}
 		$blob .= $this->getFooter();
