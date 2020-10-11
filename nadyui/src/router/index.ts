b4c48488c18a72452aaa078868c6710fa62a97ee
Jens Reidel <jens@troet.org>
2020-10-11 23:14:14 +0200

    Add websocket close handling

--- nadyui/src/router/index.ts
+++ nadyui/src/router/index.ts
@@ -10,11 +10,9 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/users",
     name: "Users",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    // Lazy-loaded component
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Users.vue"),
+      import(/* webpackChunkName: "users" */ "../views/Users.vue"),
   },
 ];
 
--- nadyui/src/store/plugins/socket.ts
+++ nadyui/src/store/plugins/socket.ts
@@ -1,10 +1,12 @@
 import { Store } from "vuex";
 
-const protocol = window.location.protocol == "https:" ? "wss" : "ws";
-const client = new WebSocket(`${protocol}://${window.location.host}/events`);
-
 export default function createWebSocketPlugin() {
   return function (store: Store<unknown>): void {
+    const protocol = window.location.protocol == "https:" ? "wss" : "ws";
+    const client = new WebSocket(
+      `${protocol}://${window.location.host}/events`
+    );
+
     //subscribe to events
     client.onopen = function (_e: Event) {
       client.send('{"command": "subscribe", "data": {"events": ["*"]}}');
@@ -13,6 +15,10 @@ export default function createWebSocketPlugin() {
 
     client.onmessage = function (e: MessageEvent<string>) {
       store.dispatch("websocketData", e.data);
+    };
+
+    client.onclose = function (e: CloseEvent) {
+      store.dispatch("websocketClose", e.code);
     };
   };
 }
