4b0d4ddece0e4cb777ab864802993a5430e157c4
Nadyita <nadyita@hodorraid.org>
2020-11-17 18:11:45 +0100

    Also download the required VC redist lib

--- install-php.ps1
+++ install-php.ps1
@@ -2,6 +2,8 @@ mkdir php
 Set-Location php
 Invoke-WebRequest -Uri "https://windows.php.net/downloads/releases/php-7.4.12-nts-Win32-vc15-x64.zip" -OutFile php.zip
 Expand-Archive -LiteralPath "php.zip" -DestinationPath "."
+Invoke-WebRequest -Uri "https://aka.ms/vs/16/release/VC_redist.x64.exe" -OutFile vc.exe
+.\vc.exe /silent
 $CWD = Get-Location
 [Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", "User") + ";$CWD", "User")
 Set-Location ..
