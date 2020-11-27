8cf8903f974a44a1b0f2f0a5d6d9d94f7bd872df
Nadyita <nadyita@hodorraid.org>
2020-11-27 11:01:41 +0100

    Fall back to buffered stdin if no readline

--- src/Core/StdinHandler.php
+++ src/Core/StdinHandler.php
@@ -17,16 +17,23 @@ class StdinHandler {
 	public SocketManager $socketManager;
 
 	/** @Inject */
+	public EventManager $eventManager;
+
+	/** @Inject */
 	public CommandManager $commandManager;
 
 	/** @Inject */
 	public Nadybot $chatBot;
+
+	/** @Inject */
+	public Timer $timer;
 
 	/** @Logger */
 	public LoggerWrapper $logger;
 
 	public SocketNotifier $notifier;
 	public $socket;
+	public bool $useReadline = false;
 
 	public function getCacheFile(): string {
 		if (isset($_SERVER["XDG_CACHE_HOME"])) {
@@ -56,36 +63,77 @@ class StdinHandler {
 		return readline_write_history($file);
 	}
 
-	/**
-	 * @Setup
-	 */
+	/** @Setup */
 	public function setup(): void {
+		$this->eventManager->register(
+			'StdinHandler',
+			'connect',
+			"stdinhandler.setupConsole",
+			"Initialize the StdIn console",
+			null,
+			1,
+		);
+	}
+
+	/**
+	 * The console should not appear before we are really online
+	 */
+	public function setupConsole(): void {
 		if (!$this->chatBot->vars["enable_console_client"]) {
 			return;
 		}
-		if (!function_exists('readline_callback_handler_install')) {
-			$this->logger->log('WARN', 'readline not supported on this platform, disabling stdin');
-			return;
+		$this->useReadline = function_exists('readline_callback_handler_install');
+		if (!$this->useReadline) {
+			$this->logger->log('WARN', 'readline not supported on this platform, using basic console');
+			$callback = [$this, "processStdin"];
+		} else {
+			$callback = function(): void {
+				readline_callback_read_char();
+			};
 		}
 		$this->loadHistory();
 		$this->socket = STDIN;
 		$this->notifier = new SocketNotifier(
 			$this->socket,
 			SocketNotifier::ACTIVITY_READ,
-			function() {
-				readline_callback_read_char();
-			}
+			$callback,
 		);
-		$this->socketManager->addSocketNotifier($this->notifier);
-		readline_callback_handler_install('> ', [$this, 'processStdin']);
+		$this->timer->callLater(1, function(): void {
+			$this->logger->log('INFO', "StdIn console activated, accepting commands");
+			$this->socketManager->addSocketNotifier($this->notifier);
+			if ($this->useReadline) {
+				readline_callback_handler_install('> ', [$this, 'processLine']);
+			} else {
+				echo("> ");
+			}
+		});
 	}
 
-	public function processStdin(?string $line): void {
+	/**
+	 * Handle data arriving on stdin
+	 */
+	public function processStdin(): void {
+		if (feof($this->socket)) {
+			echo("EOF received, closing console.\n");
+			@fclose($this->socket);
+			$this->socketManager->removeSocketNotifier($this->notifier);
+			return;
+		}
+		$line = fgets($this->socket);
+		if ($line !== false) {
+			$this->processLine(trim($line));
+			echo("> ");
+		}
+	}
+
+	public function processLine(?string $line): void {
 		if ($line === null || trim($line) === '') {
 			return;
 		}
-		readline_add_history($line);
-		$this->saveHistory();
+		if ($this->useReadline) {
+			readline_add_history($line);
+			$this->saveHistory();
+		}
 		$handler = new StdinCommandReply($this->chatBot);
 		$this->commandManager->process("msg", $line, $this->chatBot->vars["SuperAdmin"], $handler);
 	}
