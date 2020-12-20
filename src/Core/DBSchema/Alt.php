53fb7caa008ed76cadf440ba62b699b5012e2619
Nadyita <nadyita@hodorraid.org>
2020-12-20 19:48:42 +0100

    Bring in sync with SQL schema

--- src/Core/DBSchema/Alt.php
+++ src/Core/DBSchema/Alt.php
@@ -7,5 +7,7 @@ use Nadybot\Core\DBRow;
 class Alt extends DBRow {
 	public string $alt;
 	public ?string $main = null;
-	public ?bool $validated = false;
+	public ?bool $validated_by_main = false;
+	public ?bool $validated_by_alt = false;
+	public ?string $added_via = null;
 }
