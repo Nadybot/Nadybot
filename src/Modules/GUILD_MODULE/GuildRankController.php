6c938926ea3e40e0af2e8c247179c3797fdb4bb8
Nadyita <nadyita@hodorraid.org>
2021-01-06 22:53:56 +0100

    First working prototype

--- src/Modules/GUILD_MODULE/GuildRankController.php
+++ src/Modules/GUILD_MODULE/GuildRankController.php
@@ -3,13 +3,12 @@
 namespace Nadybot\Modules\GUILD_MODULE;
 
 use Nadybot\Core\{
+	AccessManager,
 	CommandReply,
 	DB,
 	Nadybot,
 	SettingManager,
-	SQLException,
 	Text,
-	Util,
 };
 use Nadybot\Core\Modules\PLAYER_LOOKUP\Guild;
 use Nadybot\Core\Modules\PLAYER_LOOKUP\GuildManager;
@@ -49,6 +48,9 @@ class GuildRankController {
 	public Nadybot $chatBot;
 
 	/** @Inject */
+	public AccessManager $accessManager;
+
+	/** @Inject */
 	public SettingManager $settingManager;
 
 	/** @Inject */
@@ -86,24 +88,165 @@ class GuildRankController {
 	 * @return OrgRankMapping[]
 	 */
 	public function getMappings(): array {
+		/** @var OrgRankMapping[] */
 		$ranks = $this->db->fetchAll(
 			OrgRankMapping::class,
-			"SELECT * FROM `org_rank_mapping_<myname>` ORDER BY `min_rank` DESC"
+			"SELECT * FROM `org_rank_mapping_<myname>` ORDER BY `min_rank` ASC"
 		);
 		return $ranks;
+	}
+
+	public function getEffectiveAccessLevel(int $rank): string {
+		/** @var OrgRankMapping */
+		$rank = $this->db->fetch(
+			OrgRankMapping::class,
+			"SELECT * FROM `org_rank_mapping_<myname>` WHERE `min_rank` >= ? ORDER BY `min_rank` ASC",
+			$rank
+		);
+		return $rank ? $rank->access_level : "guild";
 	}
 
 	/**
 	 * @HandlesCommand("maprank")
 	 * @Matches("/^maprank$/i")
 	 */
-	public function maprankCommand(string $message, string $channel, string $sender, CommandReply $sendto, array $args) {
+	public function maprankListCommand(string $message, string $channel, string $sender, CommandReply $sendto, array $args) {
+		$this->guildManager->getByIdAsync(
+			$this->chatBot->vars["my_guild_id"],
+			null,
+			false,
+			[$this, "displayRankMappings"],
+			$sendto
+		);
+	}
+
+	public function displayRankMappings(?Guild $guild, CommandReply $sendto): void {
 		$maps = $this->getMappings();
+		$mapKeys = array_reduce(
+			$maps,
+			function(array $carry, OrgRankMapping $m): array {
+				$carry[$m->min_rank] = true;
+				return $carry;
+			},
+			[]
+		);
+		$ranks = $this->orglistController->getOrgRanks($guild->governing_form);
 		if (!count($maps)) {
 			$sendto->reply("There are currently no org rank to bot rank mappings defined.");
 			return;
 		}
-		$blob = "";
+		$blob = "<header2>Mapped ranks<end>\n";
+		foreach ($ranks as $rank => $rankName) {
+			$accessLevel = $this->getEffectiveAccessLevel($rank);
+			$blob .= "<tab>".
+				"{$rank} - {$rankName}: ".
+				"<highlight>".
+				$this->accessManager->getDisplayName($accessLevel).
+				"<end>";
+			if (isset($mapKeys[$rank])) {
+				$blob .= " [" . $this->text->makeChatcmd("remove", "/tell <myname> maprank del {$rank}") . "]";
+			}
+			$blob .= "\n";
+		}
+		$msg = $this->text->makeBlob("Defined mappings (" . count($maps) . ")", $blob);
+		$sendto->reply($msg);
+	}
+
+	/**
+	 * @HandlesCommand("maprank")
+	 * @Matches("/^maprank (\d+)(?: to)? (.+)$/i")
+	 */
+	public function maprankCommand(string $message, string $channel, string $sender, CommandReply $sendto, array $args) {
+		$this->guildManager->getByIdAsync(
+			$this->chatBot->vars["my_guild_id"],
+			null,
+			false,
+			[$this, "setRankMapping"],
+			(int)$args[1],
+			$args[2],
+			$sendto
+		);
+	}
+
+	public function setRankMapping(?Guild $guild, int $rank, string $accessLevel, CommandReply $sendto): void {
+		if (!isset($guild)) {
+			$sendto->reply("This org's governing form cannot be determined.");
+			return;
+		}
+		$ranks = $this->orglistController->getOrgRanks($guild->governing_form);
+		$accessLevels = $this->accessManager->getAccessLevels();
+		$accessLevel = $this->accessManager->getAccessLevel($accessLevel);
+		if (!isset($accessLevels[$accessLevel])) {
+			$sendto->reply("<highlight>{$accessLevel}<end> is not a valid access level. Please use the short form.");
+			return;
+		}
+		if (!isset($ranks[$rank])) {
+			$sendto->reply("{$guild->governing_form} doesn't have a rank #{$rank}.");
+			return;
+		}
+		$alName = $this->accessManager->getDisplayName($accessLevel);
+		$rankName = $ranks[$rank];
+
+		$rankMapping = new OrgRankMapping();
+		$rankMapping->access_level = $accessLevel;
+		$rankMapping->min_rank = $rank;
+		/** @var ?OrgRankMapping */
+		$alEntry = $this->db->fetch(
+			OrgRankMapping::class,
+			"SELECT * FROM `org_rank_mapping_<myname>` WHERE `access_level` = ?",
+			$rankMapping->access_level
+		);
+		/** @var ?OrgRankMapping */
+		$rankEntry = $this->db->fetch(
+			OrgRankMapping::class,
+			"SELECT * FROM `org_rank_mapping_<myname>` WHERE `min_rank` = ?",
+			$rankMapping->min_rank
+		);
+		if (isset($alEntry) && isset($rankEntry)) {
+			$sendto->reply("You have already assigned rank mapping for both {$alName} and {$rankName}.");
+			return;
+		}
+		if (isset($alEntry)) {
+			$this->db->update("org_rank_mapping_<myname>", "access_level", $rankMapping);
+		} elseif (isset($rankEntry)) {
+			$this->db->update("org_rank_mapping_<myname>", "min_rank", $rankMapping);
+		} else {
+			$this->db->insert("org_rank_mapping_<myname>", $rankMapping);
+		}
+		$sendto->reply("Every <highlight>{$rankName}<end> or higher will now be mapped to <highlight>{$alName}<end>.");
+	}
+
+	/**
+	 * @HandlesCommand("maprank")
+	 * @Matches("/^maprank\s+(?:del|delete|rem|remove)\s+(\d+)$/i")
+	 */
+	public function maprankDelCommand(string $message, string $channel, string $sender, CommandReply $sendto, array $args) {
+		$this->guildManager->getByIdAsync(
+			$this->chatBot->vars["my_guild_id"],
+			null,
+			false,
+			[$this, "delRankMapping"],
+			(int)$args[1],
+			$sendto
+		);
+	}
+
+	public function delRankMapping(?Guild $guild, int $rank, CommandReply $sendto): void {
+		if (!isset($guild)) {
+			$sendto->reply("This org's governing form cannot be determined.");
+			return;
+		}
+		$ranks = $this->orglistController->getOrgRanks($guild->governing_form);
+		if (!isset($ranks[$rank])) {
+			$sendto->reply("{$guild->governing_form} doesn't have a rank #{$rank}.");
+			return;
+		}
+		$deleted = $this->db->exec("DELETE FROM `org_rank_mapping_<myname>` WHERE `min_rank` = ?", $rank);
+		if ($deleted === 0) {
+			$sendto->reply("You haven't defined any access level for <highlight>{$ranks[$rank]}<end>.");
+			return;
+		}
+		$sendto->reply("The accesslevel mapping for <highlight>{$ranks[$rank]}<end> was deleted successfully.");
 	}
 
 	/**
