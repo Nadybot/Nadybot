60c3faafccc87d13e1daa2b5e435ee2d4e0092d5
Nadyita <43058421+Nadyita@users.noreply.github.com>
2021-02-01 08:31:28 +0100

    Support Packe Requirements (#107)
    Fix uninstall bug when not restarting

--- src/Modules/PACKAGE_MODULE/Package.php
+++ src/Modules/PACKAGE_MODULE/Package.php
@@ -2,14 +2,30 @@
 
 namespace Nadybot\Modules\PACKAGE_MODULE;
 
+use stdClass;
+
 class Package {
+	/** Name of the package */
 	public string $name;
+	/** Long description of the package */
 	public string $description;
+	/** Short description of the package */
 	public string $short_description;
+	/** Version is semver notation */
 	public string $version;
+	/** Name of the author */
 	public string $author;
+	/** Required bot type (Nadybot, Budabot, Tyrbot, BeBot) */
 	public string $bot_type;
+	/** Semver range of required bot version(s) */
 	public string $bot_version;
+	/** If set, name of the GitHub repo from which to get updates */
+	public ?string $github;
+	/**
+	 * Array of requirements to run the module
+	 * @var PackageRequirement[]
+	 */
+	public array $requires;
 
 	public bool $compatible = false;
 	public int $state = 0;
---
+++ src/Modules/PACKAGE_MODULE/PackageRequirement.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Nadybot\Modules\PACKAGE_MODULE;
+
+class PackageRequirement {
+	/** Name of the module/extension that's required */
+	public string $name;
+	/** The required version */
+	public string $version;
+}
