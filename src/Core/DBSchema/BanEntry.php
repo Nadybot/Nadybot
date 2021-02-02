ef7a281b6025031ed643919cd52a4a6cd1849ab0
Nadyita <43058421+Nadyita@users.noreply.github.com>
2021-02-02 11:14:58 +0100

    Add a setting to always ban all alts of a player (#108)
    And also allow "!unban all <name>" to unban all alts.

--- src/Core/DBSchema/BanEntry.php
+++ src/Core/DBSchema/BanEntry.php
@@ -5,9 +5,21 @@ namespace Nadybot\Core\DBSchema;
 use Nadybot\Core\DBRow;
 
 class BanEntry extends DBRow {
+	/** uid of the banned person */
 	public int $charid;
+
+	/** Name of the person who banned $charid */
 	public ?string $admin;
+
+	/** Unix time stamp when $charid was banned */
 	public ?int $time;
+
+	/** Reason why $charid was banned */
 	public ?string $reason;
+
+	/** Unix timestamp when the ban ends, or null/0 if never */
 	public ?int $banend;
+
+	/** @db:ignore */
+	public string $name;
 }
