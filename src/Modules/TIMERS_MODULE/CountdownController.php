80963f14ad62b67cc106a4839dfd5b807f7737ac
Nadyita <nadyita@hodorraid.org>
2021-04-12 14:03:30 +0200

    Add countdown configuratin
    * Allow default text to be set
    * Allow cooldown between 2 countdowns to be set
    * Allow specifying where countdowns via tell should go to
    
    Closes #157

--- src/Modules/TIMERS_MODULE/CountdownController.php
+++ src/Modules/TIMERS_MODULE/CountdownController.php
@@ -3,6 +3,8 @@
 namespace Nadybot\Modules\TIMERS_MODULE;
 
 use Nadybot\Core\CommandReply;
+use Nadybot\Core\Nadybot;
+use Nadybot\Core\SettingManager;
 use Nadybot\Core\Timer;
 
 /**
@@ -21,6 +23,13 @@ use Nadybot\Core\Timer;
  */
 class CountdownController {
 
+	public const CONF_CD_TELL_LOCATION = 'cd_tell_location';
+	public const CONF_CD_DEFAULT_TEXT = 'cd_default_text';
+	public const CONF_CD_COOLDOWN = 'cd_cooldown';
+
+	public const LOC_PRIV = 1;
+	public const LOC_ORG = 2;
+
 	/**
 	 * Name of the module.
 	 * Set automatically by module loader.
@@ -28,9 +37,50 @@ class CountdownController {
 	public string $moduleName;
 
 	/** @Inject */
+	public SettingManager $settingManager;
+
+	/** @Inject */
+	public Nadybot $chatBot;
+
+	/** @Inject */
 	public Timer $timer;
 
 	private int $lastCountdown = 0;
+
+	/**
+	 * @Setup
+	 */
+	public function setup(): void {
+		$this->settingManager->add(
+			$this->moduleName,
+			self::CONF_CD_TELL_LOCATION,
+			'Where to display countdowns received via tells',
+			'edit',
+			"options",
+			"1",
+			"Priv;Guild;Priv+Guild",
+			"1;2;3"
+		);
+		$this->settingManager->add(
+			$this->moduleName,
+			self::CONF_CD_DEFAULT_TEXT,
+			'Default text to say at the end of a countdown',
+			'edit',
+			"text",
+			"GO",
+		);
+		$this->settingManager->add(
+			$this->moduleName,
+			self::CONF_CD_COOLDOWN,
+			"How long is the cooldown between starting 2 countdowns",
+			"edit",
+			"time",
+			"30s",
+			"6s;15s;30s;1m;5m",
+			'',
+			"mod"
+		);
+	}
 
 	/**
 	 * @HandlesCommand("countdown")
@@ -38,15 +88,28 @@ class CountdownController {
 	 * @Matches("/^countdown (.+)$/i")
 	 */
 	public function countdownCommand(string $message, string $channel, string $sender, CommandReply $sendto, array $args): void {
-		$message = "GO";
+		$message = $this->settingManager->getString(self::CONF_CD_DEFAULT_TEXT);
 		if (count($args) === 2) {
 			$message = $args[1];
 		}
+		$cooldown = $this->settingManager->getInt(self::CONF_CD_COOLDOWN);
 
-		if ($this->lastCountdown >= (time() - 30)) {
-			$msg = "You can only start a countdown once every 30 seconds.";
+		if ($this->lastCountdown >= (time() - $cooldown)) {
+			$msg = "You can only start a countdown once every {$cooldown} seconds.";
 			$sendto->reply($msg);
 			return;
+		}
+
+		$callback = [$sendto, "reply"];
+		if ($channel === "msg") {
+			$callback = function($text): void {
+				if ($this->settingManager->getInt(self::CONF_CD_TELL_LOCATION) & self::LOC_PRIV) {
+					$this->chatBot->sendPrivate($text, true);
+				}
+				if ($this->settingManager->getInt(self::CONF_CD_TELL_LOCATION) & self::LOC_ORG) {
+					$this->chatBot->sendGuild($text, true);
+				}
+			};
 		}
 
 		$this->lastCountdown = time();
@@ -59,11 +122,11 @@ class CountdownController {
 			} else {
 				$color = "<yellow>";
 			}
-			$msg = "[$color-------&gt; $i &lt;-------<end>]";
-			$this->timer->callLater(6-$i, [$sendto, "reply"], $msg);
+			$msg = "[{$color}-------&gt; {$i} &lt;-------<end>]";
+			$this->timer->callLater(6-$i, $callback, $msg);
 		}
 
-		$msg = "[<green>------&gt; $message &lt;-------<end>]";
-		$this->timer->callLater(6, [$sendto, "reply"], $msg);
+		$msg = "[<green>------&gt; {$message} &lt;-------<end>]";
+		$this->timer->callLater(6, $callback, $msg);
 	}
 }
