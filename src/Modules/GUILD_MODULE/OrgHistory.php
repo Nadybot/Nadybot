c15bb9bab7a2b75b0b08b5c119d786de378cc6c6
Nadyita <nadyita@hodorraid.org>
2021-09-06 15:27:16 +0200

    Add /org/history API endpoint

--- src/Modules/GUILD_MODULE/OrgHistory.php
+++ src/Modules/GUILD_MODULE/OrgHistory.php
@@ -5,10 +5,21 @@ namespace Nadybot\Modules\GUILD_MODULE;
 use Nadybot\Core\DBRow;
 
 class OrgHistory extends DBRow {
+	/** Internal ID og this history entry */
 	public int $id;
+
+	/** The person doing the action */
 	public ?string $actor;
+
+	/** Optional, the person the actor is acting on */
 	public ?string $actee;
+
+	/** The action the actor is doing */
 	public ?string $action;
+
+	/** Name of the organization this action was done in */
 	public ?string $organization;
+
+	/** Timestamp when the action happened */
 	public ?int $time;
 }
