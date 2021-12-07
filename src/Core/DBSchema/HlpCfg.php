4317b70a904484eeb701665a78b4c2bfa4df4822
Nadyita <nadyita@hodorraid.org>
2021-12-07 17:14:02 +0100

    Sanitize Hlpcfg table

--- src/Core/DBSchema/HlpCfg.php
+++ src/Core/DBSchema/HlpCfg.php
@@ -6,9 +6,9 @@ use Nadybot\Core\DBRow;
 
 class HlpCfg extends DBRow {
 	public string $name;
-	public ?string $module;
-	public ?string $file;
-	public ?string $description;
-	public ?string $admin;
+	public string $module;
+	public string $file;
+	public string $description;
+	public string $admin;
 	public int $verify = 0;
 }
---
+++ src/Core/Migrations/20211207155633_SanitizeHlpcfg.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace Nadybot\Core\Migrations;
+
+use Illuminate\Database\Schema\Blueprint;
+use Nadybot\Core\DB;
+use Nadybot\Core\HelpManager;
+use Nadybot\Core\LoggerWrapper;
+use Nadybot\Core\SchemaMigration;
+
+class SanitizeHlpcfg implements SchemaMigration {
+	public function migrate(LoggerWrapper $logger, DB $db): void {
+		$table = HelpManager::DB_TABLE;
+		$db->schema()->table($table, function(Blueprint $table) {
+			$table->string("module", 50)->nullable(false)->change();
+			$table->string("file", 255)->nullable(false)->change();
+			$table->string("description", 75)->nullable(false)->change();
+			$table->string("admin", 10)->nullable(false)->change();
+			$table->integer("verify")->nullable(false)->change();
+		});
+	}
+}
