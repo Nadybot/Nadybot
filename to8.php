39c19f5b45d86dc2ae761edd4618adbd6ad0c7ff
Nadyita <nadyita@hodorraid.org>
2021-12-09 17:50:32 +0100

    WIP

--- to8.php
+++ to8.php
@@ -8,14 +8,13 @@ use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 use ReflectionClass;
 use SplFileInfo;
-use Throwable;
 
 require 'vendor/autoload.php';
 
 
 class Migrator {
 	/**
-	 * @return \RecursiveIteratorIterator<SplFileInfo>
+	 * @return RecursiveIteratorIterator<RecursiveDirectoryIterator>
 	 */
 	private function getFileIterator(): RecursiveIteratorIterator {
 		$ite = new RecursiveDirectoryIterator(__DIR__ . "/src");
@@ -29,19 +28,21 @@ class Migrator {
 
 		$classes = array_values(array_diff($newClasses, $oldClasses));
 		foreach ($classes as $class) {
-			// $this->convertClass($class);
-			$this->convertProperties($class);
+			$code = $this->convertClass($class);
+			$code = $this->convertProperties($class, $code);
+			$code = $this->convertMethods($class, $code);
+			var_dump($code);
 		}
 	}
 
-	private function convertClass(string $class): void {
+	private function convertClass(string $class): string {
 		$reflection = new ReflectionAnnotatedClass($class);
+		$code = file_get_contents($reflection->getFileName());
 		$annotations = $reflection->getAllAnnotations();
 		if (empty($annotations)) {
-			return;
+			return $code;
 		}
 		$attribs = [];
-		$code = file_get_contents($reflection->getFileName());
 		foreach ($annotations as $annotation) {
 			$annoName = class_basename($annotation);
 			$code = preg_replace(
@@ -66,7 +67,7 @@ class Migrator {
 			if (empty($keys)) {
 				$attribs []= "NCA\\$annoName";
 			} elseif ($keys === ["value"]) {
-				$attribs []= "NCA\\$annoName(" . str_replace("\\n", "\\n\".\n\t\"", json_encode($value, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_LINE_TERMINATORS)) . ")";
+				$attribs []= "NCA\\$annoName(" . str_replace("\\n", "\\n\".\n\t\"", json_encode($annotation->value, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_LINE_TERMINATORS)) . ")";
 			} else {
 				$attribs []= "NCA\\$annoName(\n\t" . join(",\n\t", $params) . "\n)";
 			}
@@ -80,19 +81,22 @@ class Migrator {
 		}
 		$classSearch = "\nclass " . class_basename($class);
 		$code = str_replace($classSearch, "\n{$attrString}{$classSearch}", $code);
-		$code = preg_replace("/\nuse /s", "\nuse Nadybot\\Core\Attributes as NCA;\nuse ", $code, 1);
+		if (strpos($code, "Attributes as NCA") === false) {
+			$code = preg_replace("/\nuse /s", "\nuse Nadybot\\Core\Attributes as NCA;\nuse ", $code, 1);
+		}
 		$code = preg_replace("/\n\/\*\*\s*\*\//", "", $code);
-		// var_dump($code);
+		return $code;
 	}
 
-	private function convertProperties(string $class): void {
+	private function convertProperties(string $class, ?string $code): string {
 		$reflection = new ReflectionAnnotatedClass($class);
+		$code ??= file_get_contents($reflection->getFileName());
 		/** @var ReflectionAnnotatedProperty[] */
 		$properties = $reflection->getProperties();
 		if (empty($properties)) {
-			return;
+			return $code;
 		}
-		$code = file_get_contents($reflection->getFileName());
+		$changed = false;
 		foreach ($properties as $property) {
 			$annotations = $property->getAllAnnotations();
 			if (empty($annotations)) {
@@ -100,6 +104,7 @@ class Migrator {
 			}
 			$attribs = [];
 			foreach ($annotations as $annotation) {
+				$changed = true;
 				$annoName = class_basename($annotation);
 				$code = preg_replace(
 					"/[ \t]@" . $annoName . "([ \t]*(?=\n)|(\(.*?\))?(?=\n| \*\/))/s",
@@ -124,7 +129,7 @@ class Migrator {
 				if (empty($keys)) {
 					$attribs []= "NCA\\$annoName";
 				} elseif ($keys === ["value"]) {
-					$attribs []= "NCA\\$annoName(" . str_replace("\\n", "\\n\".\n\t\"", json_encode($value, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_LINE_TERMINATORS)) . ")";
+					$attribs []= "NCA\\$annoName(" . str_replace("\\n", "\\n\".\n\t\"", json_encode($annotation->value, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_LINE_TERMINATORS)) . ")";
 				} else {
 					$attribs []= "NCA\\$annoName(\n\t" . join(",\n\t", $params) . "\n)";
 				}
@@ -143,11 +148,86 @@ class Migrator {
 				1
 			);
 		}
-		if (strpos($code, "Attributes as NCU") === false) {
+		if (!$changed) {
+			return $code;
+		}
+		if (strpos($code, "Attributes as NCA") === false) {
 			$code = preg_replace("/\nuse /s", "\nuse Nadybot\\Core\Attributes as NCA;\nuse ", $code, 1);
 		}
 		$code = preg_replace("/\n[ \t]*\/\*\*[\s*]*\*\//", "", $code);
-		var_dump($code);
+		return $code;
+	}
+
+	private function convertMethods(string $class, ?string $code): string {
+		$reflection = new ReflectionAnnotatedClass($class);
+		$code ??= file_get_contents($reflection->getFileName());
+		/** @var ReflectionAnnotatedProperty[] */
+		$methods = $reflection->getMethods();
+		if (empty($methods)) {
+			return $code;
+		}
+		$changed = false;
+		foreach ($methods as $method) {
+			$annotations = $method->getAllAnnotations();
+			if (empty($annotations)) {
+				continue;
+			}
+			$attribs = [];
+			foreach ($annotations as $annotation) {
+				$changed = true;
+				$annoName = class_basename($annotation);
+				$code = preg_replace(
+					"/[ \t]@" . $annoName . "([ \t]*(?=\n)|(\(.*?\))?(?=\n| \*\/))/s",
+					"",
+					$code,
+					1
+				);
+				$params = [];
+				$keys = [];
+				$refObj = new ReflectionClass($annotation);
+				foreach ($annotation as $key => $value) {
+					if (!isset($value)) {
+						continue;
+					}
+					$refProp = $refObj->getProperty($key);
+					if ($refProp->hasDefaultValue() && $refProp->getDefaultValue() === $value) {
+						continue;
+					}
+					$keys []= $key;
+					$params []= "$key: " . str_replace("\\n", "\\n\".\n\t\t\"", json_encode($value, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_LINE_TERMINATORS));
+				}
+				if (empty($keys)) {
+					$attribs []= "NCA\\$annoName";
+				} elseif ($keys === ["value"]) {
+					$attribs []= "NCA\\$annoName(" . str_replace("\\n", "\\n\".\n\t\"", json_encode($annotation->value, JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_LINE_TERMINATORS)) . ")";
+				} else {
+					$attribs []= "NCA\\$annoName(" . join(", ", $params) . ")";
+				}
+			}
+			if (count($attribs) === 1) {
+				$attrString = "#[{$attribs[0]}]";
+			} else {
+				$attrString = "#[\n\t\t".
+					join(",\n\t\t", array_map(fn($a) => join("\n\t\t", explode("\n", $a)), $attribs)).
+				"\n\t]";
+			}
+			$code = preg_replace(
+				"/(\n[ \t]*)(public|protected|private)[ \ta-zA-Z0-9_]+function[ \t]+" . $method->getName() . "\b/s",
+				"$1{$attrString}$0",
+				$code,
+				1
+			);
+		}
+		if (!$changed) {
+			return $code;
+		}
+		if (strpos($code, "Attributes as NCA") === false) {
+			$code = preg_replace("/\nuse /s", "\nuse Nadybot\\Core\Attributes as NCA;\nuse ", $code, 1);
+		}
+		$code = preg_replace("/\n[ \t]*\/\*\*[\s*]*\*\//s", "", $code);
+		$code = preg_replace("/(\n[ \t]*\*[ \t]*)+(\n[ \t]*\*\/)/s", "$2", $code);
+		$code = preg_replace("/(\n[ \t]*\*[ \t]*){2,}/s", "$1", $code);
+		return $code;
 	}
 
 	public function run(): void {
