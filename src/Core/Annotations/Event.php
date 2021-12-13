5d6ac67c59c321724d4bf53d132a3fa023ac70a4
Nadyita <nadyita@hodorraid.org>
2021-12-13 11:38:29 +0100

    WIP

--- src/Core/Annotations/Event.php
+++ src/Core/Annotations/Event.php
@@ -5,4 +5,7 @@ namespace Nadybot\Core\Annotations;
 use Addendum\Annotation;
 
 class Event extends Annotation {
+	public string $description;
+	public ?int $defaultStatus = null;
+	public ?string $help = null;
 }
