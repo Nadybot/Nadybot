3f2bea87ae2b7429408fa0d42aa29fec190631a6
Nadyita <nadyita@hodorraid.org>
2021-12-16 14:49:24 +0100

    Fix logger name

--- src/Core/DBRow.php
+++ src/Core/DBRow.php
@@ -7,7 +7,7 @@ class DBRow {
 		$backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
 		$trace = $backtrace[1];
 		$trace2 = $backtrace[0];
-		$logger = new LoggerWrapper('DB');
+		$logger = new LoggerWrapper('Core/DB');
 		$logger->log('WARN', "Tried to get value '$value' from row that doesn't exist: " . var_export($this, true));
 		$class = "";
 		if (isset($trace['class'])) {
