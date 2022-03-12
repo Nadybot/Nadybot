8e2412db569dd4f331c54b79b8b89eed309795e5
Nadyita <nadyita@hodorraid.org>
2022-03-12 08:25:27 +0100

    Prepare for 6.0.0-alpha.2 release

--- src/Core/BotRunner.php
+++ src/Core/BotRunner.php
@@ -11,7 +11,7 @@ class BotRunner {
 	/**
 	 * Nadybot's current version
 	 */
-	public const VERSION = "6.0.0-alpha.1";
+	public const VERSION = "6.0.0-alpha.2";
 
 	/**
 	 * The command line arguments
