19a37a76fd9d5d72f20808409f7f8ea1107e569c
Nadyita <nadyita@hodorraid.org>
2022-03-31 12:06:53 +0200

    Cleanup

--- src/Modules/MASSMSG_MODULE/Migrations/20220329132503_MigrateToRoutes.php
+++ src/Modules/MASSMSG_MODULE/Migrations/20220329132503_MigrateToRoutes.php
@@ -2,12 +2,14 @@
 
 namespace Nadybot\Modules\MASSMSG_MODULE\Migrations;
 
-use Nadybot\Core\DB;
-use Nadybot\Core\DBSchema\Route;
-use Nadybot\Core\LoggerWrapper;
-use Nadybot\Core\MessageHub;
-use Nadybot\Core\Routing\Source;
-use Nadybot\Core\SchemaMigration;
+use Nadybot\Core\{
+	DB,
+	DBSchema\Route,
+	LoggerWrapper,
+	MessageHub,
+	Routing\Source,
+	SchemaMigration,
+};
 
 class MigrateToRoutes implements SchemaMigration {
 	public function migrate(LoggerWrapper $logger, DB $db): void {
