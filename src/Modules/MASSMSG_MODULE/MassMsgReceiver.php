2bbf3c586ba6dd9f6a8ccc65893e54ffb7df87f8
Nadyita <43058421+Nadyita@users.noreply.github.com>
2022-04-22 07:32:47 +0200

    mass-msgs and -invites as route dsts
    Add system(mass-message) and system(mass-invite) as route destinations,
    so you can route tara spawns to mass invites...

---
+++ src/Modules/MASSMSG_MODULE/MassMsgReceiver.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+namespace Nadybot\Modules\MASSMSG_MODULE;
+
+use Nadybot\Core\{
+	Attributes as NCA,
+	MessageHub,
+	MessageReceiver,
+	Nadybot,
+	Routing\Events\Base,
+	Routing\RoutableEvent,
+	Routing\Source,
+};
+
+/**
+ * This class accepts incoming messages and sends them out as mass messages
+ * @package Nadybot\Modules\MASSMSG_MODULE
+ */
+class MassMsgReceiver implements MessageReceiver {
+	#[NCA\Inject]
+	public Nadybot $chatBot;
+
+	#[NCA\Inject]
+	public MessageHub $messageHub;
+
+	#[NCA\Inject]
+	public MassMsgController $massMsgCtrl;
+
+	public function getChannelName(): string {
+		return Source::SYSTEM . "(mass-message)";
+	}
+
+	public function receive(RoutableEvent $event, string $destination): bool {
+		if ($event->getType() !== $event::TYPE_MESSAGE) {
+			$baseEvent = $event->data??null;
+			if (!isset($baseEvent) || !($baseEvent instanceof Base) || !isset($baseEvent->message)) {
+				return false;
+			}
+			$msg = $baseEvent->message;
+		} else {
+			$msg = (string)$event->getData();
+		}
+		$ctrl = $this->massMsgCtrl;
+		if (($ctrl->massMsgRateLimitCheck()) !== null) {
+			return false;
+		}
+		$message = "{$ctrl->massmsgColor}{$msg}<end>".
+			" :: " . $ctrl->getMassMsgOptInOutBlob();
+
+		$ctrl->massCallback([
+			MassMsgController::PREF_MSGS => function(string $name) use ($message): void {
+				$this->chatBot->sendMassTell($message, $name);
+			}
+		]);
+		return true;
+	}
+}
