984f8789b86699a3aed10b365c6e0c18e0268c08
Nadyita <nadyita@hodorraid.org>
2022-06-13 14:35:30 +0200

    Allow syncing of bans via nadynative
    Closes #371

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- You can now sync bans and unbans via nadynative
+
 ### Fix
 
 - Add back the "loot" and "auction" links to loot lists
---
+++ src/Core/Modules/BAN/SyncBanDeleteEvent.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Nadybot\Core\Modules\BAN;
+
+use Nadybot\Core\SyncEvent;
+
+class SyncBanDeleteEvent extends SyncEvent {
+	public string $type = "sync(ban-delete)";
+
+	/** uid of the banned person */
+	public int $uid;
+
+	/** name of the banned person */
+	public string $name;
+
+	/** Name of the person who banned $uid */
+	public string $unbanned_by;
+
+	/**
+	 * @return array<string,int|string|null>
+	 * @phpstan-return array{"uid":int, "name":string, "unbanned_by":?string}
+	 */
+	public function toData(): array {
+		return [
+			"uid" => $this->uid,
+			"name" => $this->name,
+			"unbanned_by" => $this->unbanned_by,
+		];
+	}
+}
