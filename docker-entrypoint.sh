29fa7e873c3c4ff15b7c83293790eb1340d25fe2
Nadyita <nadyita@hodorraid.org>
2023-02-09 11:31:38 +0100

    Support 1 yes true in Docker
    When configuring a bool value, "1", "yes" and "true"
    are now considered true, regardless of where they
    are going to be used.
    CONFIG_USE_PROXY=true works now.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,15 @@ errorMessage() {
 	exit 1
 }
 
+checkBool() {
+	value=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+	if [ "$value" = "1" ] || [ "$value" = "true" ] || [ "$value" = "yes" ]; then
+		echo "$2"
+	else
+		echo "$3"
+	fi
+}
+
 [ -z "$CONFIG_LOGIN" ] && errorMessage "You have to specify the login by setting \$CONFIG_LOGIN"
 [ -z "$CONFIG_PASSWORD" ] && errorMessage "You have to specify the password by setting \$CONFIG_PASSWORD"
 [ -z "$CONFIG_BOTNAME" ] && errorMessage "You have to specify the name of the bot by setting \$CONFIG_BOTNAME"
@@ -38,16 +47,16 @@ cat > /tmp/config.php << DONE
 \$vars['DB Host'] = "$CONFIG_DB_HOST";
 \$vars['DB username'] = "$CONFIG_DB_USER";
 \$vars['DB password'] = "$CONFIG_DB_PASS";
-\$vars['show_aoml_markup'] = ${CONFIG_SHOW_AOML_MARKUP:-0};
+\$vars['show_aoml_markup'] = $(checkBool "${CONFIG_SHOW_AOML_MARKUP:-0}" 1 0);
 \$vars['cachefolder'] = "${CONFIG_CACHEFOLDER:-./cache/}";
 \$vars['default_module_status'] = ${CONFIG_DEFAULT_MODULE_STATUS:-0};
-\$vars['use_proxy'] = ${CONFIG_USE_PROXY:-0};
-\$vars['enable_console_client'] = ${CONFIG_ENABLE_CONSOLE:-0};
-\$vars['enable_package_module'] = ${CONFIG_ENABLE_PACKAGE_MODULE:-0};
+\$vars['use_proxy'] = $(checkBool "${CONFIG_USE_PROXY:-0}" 1 0);
+\$vars['enable_console_client'] = $(checkBool "${CONFIG_ENABLE_CONSOLE:-0}" 1 0);
+\$vars['enable_package_module'] = $(checkBool "${CONFIG_ENABLE_PACKAGE_MODULE:-0}" 1 0);
 \$vars['proxy_server'] = "${CONFIG_PROXY_SERVER:-127.0.0.1}";
 \$vars['proxy_port'] = ${CONFIG_PROXY_PORT:-9993};
 \$vars['API Port'] = ${CONFIG_API_PORT:-5250};
-\$vars['auto_unfreeze'] = ${CONFIG_AUTO_UNFREEZE:-false};
+\$vars['auto_unfreeze'] = $(checkBool "${CONFIG_AUTO_UNFREEZE:-false}" true false);
 \$vars['auto_unfreeze_login'] = ${CONFIG_AUTO_UNFREEZE_LOGIN:-null};
 \$vars['auto_unfreeze_password'] = ${CONFIG_AUTO_UNFREEZE_PASSWORD:-null};
 
@@ -68,7 +77,7 @@ DONE
 sed -e "s/\"\*\": \"notice\"/\"*\": \"${CONFIG_LOG_LEVEL:-notice}\"/" conf/logging.json > /tmp/logging.json
 
 if [ -e /proxy/aochatproxy ] \
-	&& [ "${CONFIG_USE_PROXY:-0}" = "1" ] \
+	&& [ "$(checkBool "${CONFIG_USE_PROXY:-0}" 1 0)" = "1" ] \
 	&& [ -n "${PROXY_CHARNAME_1:-}" ] \
 	&& [ -n "${PROXY_USERNAME_1:-}" ] \
 	&& [ -n "${PROXY_PASSWORD_1:-}" ]; then
@@ -78,21 +87,15 @@ if [ -e /proxy/aochatproxy ] \
 	elif [ "${CONFIG_DIMENSION:-5}" = "6" ]; then
 		FC_PORT=7106
 	fi
-	SPAM_BOT_SUPPORT="false";
-	[ "${PROXY_SPAM_BOT_SUPPORT:-0}" = "1" ] && SPAM_BOT_SUPPORT="true"
-	SEND_TELLS_OVER_MAIN="true";
-	[ "${PROXY_SEND_TELLS_OVER_MAIN:-1}" = "0" ] && SEND_TELLS_OVER_MAIN="false"
-	RELAY_WORKER_TELLS="true";
-	[ "${PROXY_RELAY_WORKER_TELLS:-1}" = "0" ] && RELAY_WORKER_TELLS="false"
 	cat > /tmp/config.json <<-DONE
 		{
 		    "rust_log": "${PROXY_LOGLEVEL:-info}",
 		    "port_number": ${CONFIG_PROXY_PORT:-9993},
 		    "server_address": "chat.d1.funcom.com:${FC_PORT}",
-		    "spam_bot_support": ${SPAM_BOT_SUPPORT},
-		    "send_tells_over_main": ${SEND_TELLS_OVER_MAIN},
-		    "relay_worker_tells": ${RELAY_WORKER_TELLS},
-		    "auto_unfreeze_accounts": ${CONFIG_AUTO_UNFREEZE:-false},
+		    "spam_bot_support": $(checkBool "${PROXY_SPAM_BOT_SUPPORT:-0}" true false),
+		    "send_tells_over_main": $(checkBool "${PROXY_SEND_TELLS_OVER_MAIN:-1}" true false),
+		    "relay_worker_tells": $(checkBool "${PROXY_RELAY_WORKER_TELLS:-1}" true false),
+		    "auto_unfreeze_accounts": $(checkBool "${CONFIG_AUTO_UNFREEZE:-false}" true false),
 		    "accounts": [
 	DONE
 	SUFFIX=1
@@ -127,6 +130,7 @@ if [ -e /proxy/aochatproxy ] \
 		    ]
 		}
 	DONE
+	
 	cd /proxy || exit
 	(/usr/bin/env RUST_BACKTRACE=full /proxy/aochatproxy /tmp/config.json 2>&1| stdbuf -i0 -o0 -e0 sed -e 's/^[^ ]* \([A-Z]*\) .*\]/[PROXY:\1]/') &
 	cd /nadybot || exit
