e51584738986ddfed68c53844dde389ee797f800
Nadyita <nadyita@hodorraid.org>
2024-09-01 09:59:36 +0200

    Prepare 6.2.10 release

--- src/Core/BotRunner.php
+++ src/Core/BotRunner.php
@@ -26,7 +26,7 @@ use Throwable;
 
 class BotRunner {
 	/** Nadybot's current version */
-	public const VERSION = "6.2.9";
+	public const VERSION = "6.2.10";
 
 	public const AMP_FS_HANDLER = 'amp_fs_handler';
 
