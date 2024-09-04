73ca3d2d2defb33b301d097c257df99b76b3d777
Nadyita <nadyita@hodorraid.org>
2024-09-04 22:05:35 +0200

    The http_timeout parameter is no longer needed
    Every request has a custom timeout anyway,
    and doesn't block anything anymore.

--- src/Core/Modules/SYSTEM/SystemController.php
+++ src/Core/Modules/SYSTEM/SystemController.php
@@ -119,10 +119,6 @@ class SystemController extends ModuleInstance implements MessageEmitter {
 	#[NCA\Setting\Boolean]
 	public bool $addHeaderRanges = false;
 
-	/** Max time to wait for response from making http queries */
-	#[NCA\Setting\Time(options: ['1s', '2s', '5s', '10s', '30s'])]
-	public int $httpTimeout = 10;
-
 	/** Database version */
 	#[NCA\Setting\Text(mode: SettingMode::NoEdit)]
 	public string $version = '0';
