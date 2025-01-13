5a1cfd745fc06adbca8513ca78916ddc1a4d8a8e
Nadyita <nadyita@hodorraid.org>
2025-01-13 09:23:46 +0100

    Cleanup some now unused nadyui files

--- nadyui/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-EXECUTABLE=$(which podman 2>/dev/null || echo "Podman not found" && exit 1)
-$EXECUTABLE build -t nadyui:latest -v $(pwd):/build/:Z --no-cache .
-$EXECUTABLE rmi -f nadyui:latest
