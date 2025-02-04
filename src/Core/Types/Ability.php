6e206f6ade767f8a5684970747a63df247e3c339
Nadyita <nadyita@hodorraid.org>
2025-02-04 18:40:16 +0100

    Support Ability-IDs

--- src/Core/Types/Ability.php
+++ src/Core/Types/Ability.php
@@ -40,6 +40,29 @@ enum Ability: string implements EnumParameterInterface {
 		return '(agi|agl|int|psy|sta|stm|str|sen|sns)\w*';
 	}
 
+	public function getID(): int {
+		return match ($this) {
+			self::Strength => 16,
+			self::Agility => 17,
+			self::Stamina => 18,
+			self::Intelligence => 19,
+			self::Sense => 20,
+			self::Psychic => 21,
+		};
+	}
+
+	public function byID(int $id): self {
+		return match ($id) {
+			16 => self::Strength,
+			17 => self::Agility,
+			18 => self::Stamina,
+			19 => self::Intelligence,
+			20 => self::Sense,
+			21 => self::Psychic,
+			default => throw new ValueError("Unknown ability {$id}"),
+		};
+	}
+
 	case Agility = 'agi';
 	case Intelligence = 'int';
 	case Psychic = 'psy';
