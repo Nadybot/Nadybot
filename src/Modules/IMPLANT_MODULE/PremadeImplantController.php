9fbfe5485a97f73e2ab0711aa8a95c930f025d3e
Nadyita <nadyita@hodorraid.org>
2025-03-05 12:18:05 +0100

    Add tests for implant designer

--- src/Modules/IMPLANT_MODULE/PremadeImplantController.php
+++ src/Modules/IMPLANT_MODULE/PremadeImplantController.php
@@ -119,7 +119,7 @@ class PremadeImplantController extends ModuleInstance {
 	}
 
 	public function getFormattedLine(PremadeSearchResult $implant): string {
-		return "<tab><highlight>{$implant->profession->name}<end> ({$implant->ability->name})\n".
+		return "<tab><highlight>{$implant->profession->value}<end> ({$implant->ability->name})\n".
 			"<tab>S: {$implant->shiny}\n".
 			"<tab>B: {$implant->bright}\n".
 			"<tab>F: {$implant->faded}\n\n";
